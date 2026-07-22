ea2b8caf7bbc3ea62fad2e8b1192b13e060969d6
Hoodh Waleedh <hoodhwaleedh@gmail.com>
2026-07-21 21:13:41 -0400

    setup for update event response, added appropriate exceptions for now

--- src/main/java/com/hwal/tickets/services/EventService.java
+++ src/main/java/com/hwal/tickets/services/EventService.java
@@ -2,6 +2,7 @@ package com.hwal.tickets.services;
 
 
 import com.hwal.tickets.domain.CreateEventRequest;
+import com.hwal.tickets.domain.UpdateEventRequest;
 import com.hwal.tickets.domain.entities.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,4 +17,5 @@ public interface EventService {
     //Page allows us to limit the amount of events retrieved (instead of retrieving a lot of events and overflowing page)
     Page<Event> listEventsForOrganizer(UUID organizerID, Pageable pageable);
     Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
+    Event updateEventForOrganizer(UUID organizerId, UUID id, UpdateEventRequest event);
 }
